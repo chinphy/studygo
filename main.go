b771dfa913033e0cf723bb495d9896ea376f7021
chinphy <chinphy520@gmail.com>
2019-02-11 14:16:25 +0800

    m

--- main.go
+++ main.go
@@ -1,69 +1,7 @@
 package main
 
-// Person 请假人
-type Person struct {
-	name   string
-	dayoff int
-	reason string
-	leader Manager
-}
-
-func (c Person) ReuqestD() {
-
-}
-
-// ManagerInterface 管理人员接口
-type ManagerInterface interface {
-	HandleRequest()
-}
-
-// Manager 管理人员基类
-type Manager struct {
-	Name        string
-	Title       string
-	NextHandler ManagerInterface
-}
-
-func (c Manager) HandleRequest() string {
-	return ""
-}
-
-// Supervisor 直接上级
-type Supervisor struct {
-	Manager
-}
-
-func (c Supervisor) HandleRequest() {
-	return
-}
-
-// DepartmentManager 部门总监
-type DepartmentManager struct {
-	Manager
-}
-
-func (c DepartmentManager) HandleRequest() {
-	return
-}
-
-// CEO ceo
-type CEO struct {
-	Manager
-}
-
-func (c CEO) HandleRequest() {
-	return
-}
-
-// Administrator 行政
-type Administrator struct {
-	Manager
-}
-
-func (c Administrator) HandleRequest() {
-	return
-}
+import "fmt"
 
 func main() {
-
+	fmt.Println("hello")
 }
