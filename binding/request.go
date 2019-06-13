64ee257bfab554d26677ff1bbf19d0fc17a17857
chinphy <chinphy520@gmail.com>
2019-06-13 11:01:15 +0800

    gin

--- binding/request.go
+++
@@ -1,6 +0,0 @@
-package binding
-
-type Login struct {
-	Username string `json:"username" form:"username" binding:"required"`
-	Password string `json:"password" form:"password" binding:"required"`
-}
